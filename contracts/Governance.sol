77cd2cfa57dc5fb17051afe3a0a34a72c1e830b1
PanaW <pana.wang@outlook.com>
2023-08-07 18:45:23 +0800

    init project

---
+++ contracts/Governance.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+
+/**
+ * 1. 允许一个人对不同的选项进行投票
+ * 2. 允许任何人在满足某条件的情况下创建一个新的投票
+ * 3. 对旧投票合约的兼容: 数据, 接口
+ */
+
+contract Governance {
+
+}
